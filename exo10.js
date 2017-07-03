6f7be42ffd7523f54ca5e49da0ac1ddbad903027
popschool <popschool@debian>
2017-07-03 15:07:36 +0200

    exercices

---
+++ exo10.js
@@ -0,0 +1,6 @@
+function addition() {
+  var calculatrice1 = document.getElementById('calculatrice1').value;
+  var nombre2 = document.getElementById('nombre2').value;
+
+document.getElementById("resultats").innerHTML = parseInt(calculatrice1) + parseInt(nombre2);
+}
---
+++ scriptform.js
@@ -0,0 +1,12 @@
+var printform = function(){
+  var box = document.getElementById("box");
+  box.innerHTML =
+  "Nom:" + document.getElementById("lastname").value + "<br>" +
+  "Prénom:" + document.getElementById("firstname").value + "<br>" +
+  "Age:" + document.getElementById("age").value + "<br>" +
+  "Adresse:" + document.getElementById("add").value + "<br>";
+
+
+
+
+}
